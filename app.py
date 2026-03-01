91050ebe7c39b841d4889c4363548cb15f77646d
vaibhavswami13 <ec2-user@ip-172-31-40-221.ap-south-1.compute.internal>
2026-03-01 04:23:15 +0000

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD Working Successfully!!, test1test22"
+    return "DevOps CI/CD Working Successfully!!, test1test23"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
