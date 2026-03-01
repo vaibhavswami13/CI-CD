323ba068c44baf87f9ecf03b77607271fe3d85f0
Vaibhav Swami <vaibhavswami14061995@gmail.com>
2026-03-01 09:59:11 +0530

    Update success message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD Working Successfully!!, test1test23"
+    return "DevOps CI/CD Working Successfully!!, test1test24"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
