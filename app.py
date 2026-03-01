fcf792c930690cadbf0fe68fb8208ea603465e99
Vaibhav Swami <vaibhavswami14061995@gmail.com>
2026-03-01 19:32:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 def home():
 <<<<<<< HEAD
     return "DevOps CI/CD Working Successfully!!, test1test27"
-=======
-    return "DevOps CI/CD Working Successfully!!, test1test25"
->>>>>>> refs/remotes/origin/main
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
