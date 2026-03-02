26c04e67c7929c1c93cdc00a545b11d9051e29f0
vaibhavswami13 <ec2-user@ip-172-31-40-221.ap-south-1.compute.internal>
2026-03-02 07:04:46 +0000

    updated files

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from app1  import demo
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return (demo())
 
-   # return "DevOps CI/CD Working Successfully!!, test1test28"
+def home():
+
+ return "DevOps CI/CD Working Successfully!!, test1test28"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
