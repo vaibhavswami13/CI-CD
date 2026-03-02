19c9978dc8d97720370481b0093b38c3ea0b57f0
Vaibhav Swami <vaibhavswami14061995@gmail.com>
2026-03-02 12:30:41 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-    return "Welcome to demo docker"
+    return "Welcome to demo of docker"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
