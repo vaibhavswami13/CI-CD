e062f3b43aae1c458ada667bfc670bb938ef7f22
vaibhavswami13 <ec2-user@ip-172-31-40-221.ap-south-1.compute.internal>
2026-03-02 03:40:56 +0000

    new

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from demo import demo
+from app1  import demo
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return (demo())
 
-    return "DevOps CI/CD Working Successfully!!, test1test28"
+   # return "DevOps CI/CD Working Successfully!!, test1test28"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-    #app1.py
-
-    def demo():
-        return"welcome to demo!"
