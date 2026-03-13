03f412246d6568a3ece986dd53de3a92b93b3043
vaibhavswami13 <vaibhavswami14061995@gmail.com>
2026-03-13 06:10:17 +0000

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def home():
 
- return "DevOps CI/CD Working Successfully!!, test1test29"
+ return "DevOps CI/CD Working Successfully!!, test1test30"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000) 
